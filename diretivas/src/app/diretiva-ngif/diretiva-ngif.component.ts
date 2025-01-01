79f9ea61eb62441b18f958b9fca24e3258898253
Vitor Eble <vitoreble06@gmail.com>
2025-01-01 17:22:21 -0300

    ngif

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   standalone: false,
-  
+
   templateUrl: './diretiva-ngif.component.html',
   styleUrl: './diretiva-ngif.component.scss'
 })
 export class DiretivaNgifComponent {
+  cursos: string[] = ['angular'];
 
+  mostrarCursos: boolean = false;
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos
+  }
 }
