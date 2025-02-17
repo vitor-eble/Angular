d46ada24f13f4425a01b27038b9fb13da1692b4e
Vitor Eble <vitoreble06@gmail.com>
2025-02-17 18:04:25 -0300

    aula 43 sobre pipes

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
