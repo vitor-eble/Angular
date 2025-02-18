82d03b5da2878a05f2d2de32f9a61c920b24420f
Vitor Eble <vitoreble06@gmail.com>
2025-02-18 14:52:42 -0300

    aula 45: locale nos pipes

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,18 @@ import { CamelCasePipe } from './camel-case.pipe';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService: any) => settingsService.getLocale()
+    }
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: 'pt-BR',
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
