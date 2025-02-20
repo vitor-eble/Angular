009ac2f393b0f0ffc57677c2521905f5af711df6
Vitor Eble <vitoreble06@gmail.com>
2025-02-20 16:03:40 -0300

    exclui a app routing module

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -21,7 +20,6 @@ import { MatIcon } from '@angular/material/icon';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MatSlideToggleModule,
     MatToolbar,
     MatToolbarRow,
