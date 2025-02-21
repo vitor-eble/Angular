062ecd93adb527d208279fb625232c061c06d6a7
Vitor Eble <vitoreble06@gmail.com>
2025-02-21 14:12:02 -0300

    aula 54: escutando mudanças nos parametros das rota

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -7,12 +8,30 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './curso-detalhe.component.html',
   styleUrl: './curso-detalhe.component.css'
 })
-export class CursoDetalheComponent {
-
+export class CursoDetalheComponent implements OnInit, OnDestroy {
   id: string = '';
+  inscricao: Subscription = new Subscription()
 
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id']
+    // this.id = this.route.snapshot.params['id'];
+    // console.log(this.route);
+  }
 
+  ngOnInit() {
+    // this.inscricao = this.route.params.subscribe(
+    //   (params: any) => {
+    //     this.id = params['id']
+    //   }
+    // );
+
+    this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id']
+      }
+    )
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe()
   }
 }
