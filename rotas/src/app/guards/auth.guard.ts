290eb85a10d8ba79781edb3fdebeb486839d912e
Vitor Eble <vitoreble06@gmail.com>
2025-03-24 15:22:36 -0300

    aula 69: canload

--- rotas/src/app/guards/auth.guard.ts
+++ rotas/src/app/guards/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, MaybeAsync, Route, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from '../login/auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -20,6 +20,19 @@ export class AuthGuard implements CanActivate {
     this.router.navigate(['/login']);     //se nao estiver autenticado, redireciona para a pagina de login
 
     return false; //retorna false
+  }
+
+  private verificarAcesso() {
+    if(this.authService.UsuarioEstaAutenticado()){
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
+  };
+
+  canLoad(route: Route): MaybeAsync<any>{
+    return this.verificarAcesso();
   }
 
 }
