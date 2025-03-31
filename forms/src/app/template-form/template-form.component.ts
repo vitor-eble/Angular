f2b80265b1e43aa0fa8e2429db77d18a31c35421
Vitor Eble <vitoreble06@gmail.com>
2025-03-31 17:35:10 -0300

    aula 86: populando campos com setvalue e patchvalue

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
   selector: 'app-template-form',
@@ -34,18 +35,54 @@ export class TemplateFormComponent {
     }
   }
 
-  consultaCEP(cep: any){
+  consultaCEP(cep: any, form: any){
     cep = cep.replace(/\D/g, '');
-
     if(cep != ""){
       var validacep = /^[0-9]{8}$/;
-
       if(validacep.test(cep)){
-
+        this.resetaDadosForm(form)
         this.http.get(`//viacep.com.br/ws/${cep}/json`)
-        .subscribe(dados => console.log(dados));
+        .subscribe(dados => this.populaDadosForm(dados, form));
       }
     }
+  }
 
+  populaDadosForm(dados: any, formulario: any){
+    /*formulario.setValue({
+      nameInput: formulario.value.nameInput,
+      emailInput: formulario.value.emailInput,
+      endereco: {
+        cepInput: dados.cep,
+        numeroInput: '',
+        complementoInput: dados.complemento,
+        ruaInput: dados.logradouro,
+        bairroInput: dados.bairro,
+        cidadeInput: dados.localidade,
+        estadoInput: dados.uf
+      }
+    });*/
+
+    formulario.form.patchValue({
+      endereco: {
+        //cepInput: dados.cep,
+        complementoInput: dados.complemento,
+        ruaInput: dados.logradouro,
+        bairroInput: dados.bairro,
+        cidadeInput: dados.localidade,
+        estadoInput: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(formulario: any){
+    formulario.form.patchValue({
+      endereco: {
+        complementoInput: null,
+        ruaInput: null,
+        bairroInput: null,
+        cidadeInput: null,
+        estadoInput: null
+      }
+    });
   }
 }
